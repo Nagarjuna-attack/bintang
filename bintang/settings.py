9910c0f9846ae499b452b5caec0a33ab04396435
Muhafandi <garamgudang1917@gmail.com>
2021-07-27 02:45:48 +0800

    asdfasdf

--- bintang/settings.py
+++ bintang/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'bintang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bintang',
+        'USER':'postgres',
+        'PASSWORD':'dataadmin',
+        'HOST':'localhost'
     }
 }
 
