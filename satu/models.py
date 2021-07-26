600e80b4a8df75eefaaaea98ab2c2cc5fad19c57
Muhafandi <garamgudang1917@gmail.com>
2021-07-26 14:38:16 +0800

    plugin database

--- satu/models.py
+++ satu/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Fitur(models.Model):
+	name   = models.CharField(max_length=200)
+	detail = models.CharField(max_length=200)
+	icon   = models.CharField(max_length=500)
--- satu/views.py
+++ satu/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
+from .models import Fitur
 
 # Create your views here.
 def index(request):
-	context = {
-		'nama':'Amigo',
-		'umur':25,
-	}
+	fiturku = Fitur()
+	fiturku.id=1
+	fiturku.icon='ri-stack-line'
+	fiturku.name='Service'
+	fiturku.detail='Merupakan model yang digunakan untuk mendapatkan kekuatan'
 
-	return render(request,'index.html',context)
+	fiturku2 = Fitur()
+	fiturku2.id=2
+	fiturku2.icon='ri-palette-line'
+	fiturku2.name='Paket'
+	fiturku2.detail='Pesanan yang akan kami antarkan sebentar lagi, akan meluncur'
+
+	fiturku3 = Fitur()
+	fiturku3.id=3
+	fiturku3.icon='ri-command-line'
+	fiturku3.name='Kontak'
+	fiturku3.detail='Anda bisa menghubungi kami dimanapun kalian berada'
+
+	fiturku4 = Fitur()
+	fiturku4.id=4
+	fiturku4.icon='ri-fingerprint-line'
+	fiturku4.name='Tentang'
+	fiturku4.detail='Kami Melayani semua jenis jasa pengiriman kurir'
+
+	fiturs = [fiturku,fiturku2,fiturku3,fiturku4]
+
+	return render(request,'index.html',{'fiturs':fiturs})
 
 def counter(request):
 	text = request.POST['text']
